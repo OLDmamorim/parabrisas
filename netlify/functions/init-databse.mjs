4c43979176be5982d3477721c8bc97ba637de7cf
OLDmamorim <mramorim78@gmail.com>
2025-09-14 11:36:04 +0100

    Update init-databse.mjs

--- netlify/functions/init-databse.mjs
+++ netlify/functions/init-databse.mjs
@@ -1,10 +1,16 @@
-import { initAuthDatabase } from '../../init-db.mjs';
+import { initAuthDatabase } from './init-db.mjs';
 
 export const handler = async (event, context) => {
   try {
     await initAuthDatabase();
     return {
       statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      },
       body: JSON.stringify({ 
         success: true, 
         message: 'Base de dados inicializada com sucesso!' 
@@ -13,6 +19,12 @@ export const handler = async (event, context) => {
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      },
       body: JSON.stringify({ 
         error: error.message 
       })
